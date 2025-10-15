2b84559c8fc3cdd353b0ab9130da21b347a4b57a
DavinLee <davin11992@gmail.com>
2025-10-16 05:11:05 +0900

    feat: 빈 입력과 공백 입력 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,5 +6,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        if (input.trim().isEmpty()) {
+            System.out.print("결과 : " + 0);
+            return;
+        }
     }
 }
