5cd0f6e5c8d5c5633df7c2e38988dc510991b1fd
DavinLee <davin11992@gmail.com>
2025-10-19 08:59:15 +0900

    feat: 기본&커스텀 구분자 정규식으로 처리와 합계산 및 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,32 +1,51 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputExpression = Console.readLine();
 
-        if (inputExpression.trim().isEmpty()) {
+        if (inputExpression == null || inputExpression.trim().isEmpty()) {
             System.out.print("결과 : " + 0);
             return;
         }
 
         String[] numbers = splitByDelimiter(inputExpression);
-        System.out.println("numbers = " + Arrays.toString(numbers));
+        System.out.print("결과 : " + sum(numbers));
     }
 
     private static String[] splitByDelimiter(String inputExpression) {
-        String delimiter = ",|:";
-        String numbers = inputExpression;
+        inputExpression = inputExpression.replace("\\n", "\n");
 
-        if (inputExpression.startsWith("//")) {
-            int delimiterEndIndex = inputExpression.indexOf('\n');
-            delimiter = inputExpression.substring(2, delimiterEndIndex);
-            numbers = inputExpression.substring(delimiterEndIndex + 1);
+        String defaultDelimiter = ",|:";
+        String delimiter = defaultDelimiter;
+        String numbers = inputExpression; //trim()해야되나?
+
+        Pattern pattern = Pattern.compile("^//(.*?)(?:\n)(.*)$");
+        Matcher matcher = pattern.matcher(inputExpression);
+
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            numbers = matcher.group(2);
+            delimiter = defaultDelimiter + "|" + Pattern.quote(customDelimiter);
         }
 
         return numbers.split(delimiter);
+    }
+
+    private static int sum(String[] numbers) {
+        int sum = 0;
+        for (String number : numbers) {
+            int value = Integer.parseInt(number.trim());
+            if (value < 0) {
+                throw new IllegalArgumentException("음수는 입력할 수 없습니다. " + value);
+            }
+            sum += value;
+        }
+        return sum;
     }
 }
