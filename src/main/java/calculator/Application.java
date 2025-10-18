23c4b58e6353a46ce3a0b21974c0d339dfb7c495
DavinLee <davin11992@gmail.com>
2025-10-18 22:41:17 +0900

    feat: 기본&커스텀 구분자 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,14 +1,32 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = Console.readLine();
-        if (input.trim().isEmpty()) {
+        String inputExpression = Console.readLine();
+
+        if (inputExpression.trim().isEmpty()) {
             System.out.print("결과 : " + 0);
             return;
         }
+
+        String[] numbers = splitByDelimiter(inputExpression);
+        System.out.println("numbers = " + Arrays.toString(numbers));
+    }
+
+    private static String[] splitByDelimiter(String inputExpression) {
+        String delimiter = ",|:";
+        String numbers = inputExpression;
+
+        if (inputExpression.startsWith("//")) {
+            int delimiterEndIndex = inputExpression.indexOf('\n');
+            delimiter = inputExpression.substring(2, delimiterEndIndex);
+            numbers = inputExpression.substring(delimiterEndIndex + 1);
+        }
+
+        return numbers.split(delimiter);
     }
 }
